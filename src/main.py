b869b9139084d437508f5d8a38719b808945c106
Robert McDermott <robert.mcd@live.com>
2026-07-14 13:14:34 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,24 +1,34 @@
 
-#Boot Message
-print("""SpiralOS Starting...
-         SpiralOS v0.01
-         Local Mode Enabled
-      """)
-#User name defined
-user_name = input("Enter Name: ")
-#User input var defined
-user_input = input("You: ")
 
-#Edge case for no input
-if user_input == "":
+#Boot Message
+print("""
+SpiralOS Starting...
+SpiralOS v0.01
+Local Mode Enabled
+      """)
+
+
+user_name = input(f"""What is your name?
+    Name: """)
+if user_name == "":
     print("You didn't type anything.")
 
-print(f"""What is your name?
-      Name: {user_name}""")
+user_request = input(f"What can I help you with today?: ")
+
+if user_request  == "":
+    print("You didn't type anything.")
 
 
-print(f"SpiralOS heard you say: {user_input}")
+
+#Edge case for no input
+if user_request or user_name == "":
+    print("You didn't type anything.")
+
+
+
+#print(f"SpiralOS heard you say: {user_input}")
 print(f"Welcome {user_name}")
+print(f"Let's get started on {user_request}!")
 
-if user_input == "Hello":
-    print("What can I help you with today?")
+#if user_input == "Hello":
+    #print("What can I help you with today?")
