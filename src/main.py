3fa65bac5eb81098f178dc2ff9da4907d5043870
Robert McDermott <robert.mcd@live.com>
2026-07-14 11:23:21 -0400

    Update main.py
    Added first edge case.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,13 @@
 print("SpiralPS Starting...")
 
+#User input var defined
 user_input = input("You: ")
 
+#Edge case for no input
+if user_input == "":
+    print("You didn't type anything.")
+
 print("SpiralOS heard:", user_input)
+
+if user_input == "Hello":
+    print("What can I help you with today?")
