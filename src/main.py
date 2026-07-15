541dccd7d56bbea45a173da120b6389be4dfa8cd
Robert McDermott <robert.mcd@live.com>
2026-07-14 21:37:56 -0400

    Update main.py
    More code updates.
    
    Adding basic commands.

--- src/main.py
+++ src/main.py
@@ -1,32 +1,37 @@
-noinput_error = "You didn't type anything in the input."
 
-#Boot Message
-print("""
+
+# Boot Message
+boot_message = """
 SpiralOS Starting...
 SpiralOS v0.01
 Local Mode Enabled
-      """)
+      """
+running = True
+if running == True:
+    print(boot_message)
 
-
+# User info/request
 user_name = input("""What is your name?
     Name: """)
 if user_name == "":
-    print(noinput_error)
-
+    print("A name is required.")
+    
+print(f"""
+Welcome, {user_name}!
+Type "help" to see available commands.
+""")
+    
 user_request = input("What can I help you with today?: ")
-
-#Edge case for blank 
 if user_request  == "":
-    print(noinput_error)
-
-
-
-
-
-
-#print(f"SpiralOS heard you say: {user_input}")
-print(f"Welcome {user_name}")
-print(f"Let's get started on {user_request}!")
-
-#if user_input == "Hello":
-    #print("What can I help you with today?")
+    print("A request is required.")
+elif user_request == "hello":
+    print(f"Hello, {user_name}")
+elif user_request == "help":
+    print("Current Commands: hello, help, about and exit.\n")
+    print(f"Let's get started on {user_request}!")
+elif user_request == "exit":
+    running = False
+    print("System is shutting down.")
+else: 
+    user_request != ("help", "exit", "")
+    print("Invalid Command.")
