301b88353b0f292f3663d8605c8ac95d28bf5beb
Robert McDermott <robert.mcd@live.com>
2026-07-14 22:02:03 -0400

    Update main.py
    Almost done polishing and fixing initial loop.

--- src/main.py
+++ src/main.py
@@ -7,14 +7,17 @@ SpiralOS v0.01
 Local Mode Enabled
       """
       
-print(boot_message)
+
 running = True
 if running:
     print("...running...")
+    
+print(boot_message)
 
 # User info/request
-user_name = input(f"""What is your name?
-    Name: """).strip
+user_name = input("""
+What is your name?
+Name: """).strip()
 if user_name == "":
     print("A name is required.")
     running == False
@@ -24,18 +27,19 @@ Welcome, {user_name}!
 Type "help" to see available commands.
 """)
     
-user_request = input("What can I help you with today?: ").strip().lower()
-if user_request  == "":
-    print("A request is required.")
-elif user_request == "hello":
-    print(f"Hello, {user_name}")
-elif user_request == "help":
-    print("Current Commands: hello, about, help, about and exit.\n")
-elif user_request == "about":
-    print("SpiralOS is a local-first assistant project")
-elif user_request == "exit":
-    running = False
-    print("System is shutting down.")
-else: 
-    user_request != ("help", "exit", "", "about")
-    print("Invalid Command.")
+while running:
+    user_request = input("What can I help you with today?: ").strip().lower()
+    if user_request  == "":
+        print("A request is required.")
+    elif user_request == "hello":
+        print(f"Hello, {user_name}")
+    elif user_request == "help":
+        print("Current Commands: hello, help, about and exit.\n")
+    elif user_request == "about":
+        print("SpiralOS is a local-first assistant project")
+    elif user_request == "exit":
+        running = False
+        print("System is shutting down.")
+    else: 
+        user_request != ("help", "exit", "", "about")
+        print("Invalid Command.")
